5ac20998a843f4daeef6792bbc0efa3c801c9f42
Timaks <aksinya2016@gmail.com>
2024-09-06 18:05:52 +0300

    rename

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import Categories from './components/Categories'
 import Header from './components/Header'
+import Sort from './components/Sort'
 
 function App() {
   return (
@@ -11,31 +12,7 @@ function App() {
           <div class="container">
             <div class="content__top">
               <Categories />
-              <div class="sort">
-                <div class="sort__label">
-                  <svg
-                    width="10"
-                    height="6"
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                      fill="#2C2C2C"
-                    />
-                  </svg>
-                  <b>Сортировка по:</b>
-                  <span>популярности</span>
-                </div>
-                <div class="sort__popup">
-                  <ul>
-                    <li class="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
-                  </ul>
-                </div>
-              </div>
+              <Sort />
             </div>
             <h2 class="content__title">Все пиццы</h2>
             <div class="content__items">
@@ -76,7 +53,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -114,7 +91,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -152,7 +129,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -190,7 +167,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -228,7 +205,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -266,7 +243,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -304,7 +281,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
@@ -342,7 +319,7 @@ function App() {
                     <i>2</i>
                   </div>
                 </div>
-              </div>{' '}
+              </div>
               <div class="pizza-block">
                 <img
                   class="pizza-block__image"
