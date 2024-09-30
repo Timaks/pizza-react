46b9c91d24a3c3854e62925442de63018fb4c954
Timaks <aksinya2016@gmail.com>
2024-09-30 17:25:52 +0300

    закончили сортировку (по выбору-цена, популярность...)

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 function Sort() {
   const [open, setOpen] = React.useState(false)
+  const [selected, setSelected] = React.useState(0)
+  const list = ['популярности', 'цене', 'алфавиту']
+  const sortName = list[selected]
+
+  const onClickListItem = (i) => {
+    setSelected(i)
+    setOpen(false)
+  }
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -18,15 +27,22 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span onClick={()=>setOpen(!open)}>популярности</span>
+        {/* или вместо sortName ставить сразу  list[selected] */}
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
       {/* Можно использовать тернарный оператор ? :, но без if только в олну строку выражение */}
       {open && (
         <div className="sort__popup">
           <ul>
-            <li className="active">популярности</li>
-            <li>цене</li>
-            <li>алфавиту</li>
+            {list.map((name, i) => (
+              <li
+                key={i}
+                onClick={() => onClickListItem(i)}
+                className={selected === i ? 'active' : ''}
+              >
+                {name}
+              </li>
+            ))}
           </ul>
         </div>
       )}
