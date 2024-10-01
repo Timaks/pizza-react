69c66af285c6752a7378971e99231a6b124e9685
Timaks <aksinya2016@gmail.com>
2024-10-01 18:03:22 +0300

    skeleton

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Categories from './components/Categories'
 import Header from './components/Header'
 import PizzaBlock from './components/PizzaBlock'
 import Sort from './components/Sort'
-
+import Skeleton from './components/PizzaBlock/skeleton'
 // import pizzas from './assets/pizzas.json'
 function App() {
   const [items, setItems] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
   // https://66fab3a48583ac93b4098801.mockapi.io/items
 
   // Будет постоянная перерисовка, чтобы не было используем хук эффект
@@ -30,10 +31,17 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {items.map((obj) => (
-                // если в объекте названия одинаковые то сокращаем
-                <PizzaBlock key={obj.id} {...obj} />
-              ))}
+              {isLoading
+                ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+              {/* {items.map((obj) =>
+                isLoading ? (
+                  <Skeleton />
+                ) : (
+                  // если в объекте названия одинаковые то сокращаем
+                  <PizzaBlock key={obj.id} {...obj} />
+                )
+              )} */}
             </div>
           </div>
         </div>
