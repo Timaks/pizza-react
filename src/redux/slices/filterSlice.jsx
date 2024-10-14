55071f40c8b3d4d7065daaf691036978f1e2a459
Timaks <aksinya2016@gmail.com>
2024-10-14 15:52:52 +0300

    Закончили делать слайс фильтр

--- src/redux/slices/filterSlice.jsx
+++ src/redux/slices/filterSlice.jsx
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+// начальное состояние и значения по умолчанию
+// initialState - начальное состояние слайса стейта, которое будет загружено при первом запуске приложения
 const initialState = {
   categoryId: 0,
   sort: {
@@ -7,16 +9,19 @@ const initialState = {
     sortProperty: 'rating',
   },
 }
-
-export const filterSlice = createSlice({
+// сам слайс, тут логика обработки наших данных
+const filterSlice = createSlice({
   name: 'filter',
   initialState,
+  //   данные стейта изменяются при помощи редьюсеров (action-действие)
   reducers: {
-   setCategoryId(state, action) {
-    state.categoryId = action.payload
-   }
+    setCategoryId(state, action) {
+      state.categoryId = action.payload
+    },
   },
 })
-export const { increment, decrement, incrementByAmount } = filterSlice.actions
+// вытаскиваем то. что в reducers
+export const { setCategoryId } = filterSlice.actions
 
+// экспортируем reducer - база
 export default filterSlice.reducer
